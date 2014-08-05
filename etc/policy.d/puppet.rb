6b1e27236de24da615d3a2827739d12ae22f5f29
Craig Dunn <craig@craigdunn.org>
2014-08-05 12:23:43 +0200

    rename

---
+++ etc/policy.d/puppet.rb
@@ -0,0 +1,23 @@
+
+policy :puppet do
+
+  lookup :default do
+    datasource :file, {
+      :format => :yaml,
+      :docroot => "/Users/craigdunn/jacaranda/etc/data",
+      :searchpath => [
+        "#{scope[:environment]}",
+        'global',
+        'common'
+    ]
+    }
+
+    exclude :environment, "production"
+    output_filter :encryption
+    hiera_compat
+  end
+
+
+end
+
+ 
