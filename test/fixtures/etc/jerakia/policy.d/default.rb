b1518460de64fd03a89bf83fbf9dff4c130a29af
Craig Dunn <craig@craigdunn.org>
2016-03-17 10:35:33 +0100

    Cleaned up old data and added some test data for schema feature

--- test/fixtures/etc/jerakia/policy.d/default.rb
+++ test/fixtures/etc/jerakia/policy.d/default.rb
@@ -1,8 +1,8 @@
 policy :default do
   lookup :default do
     datasource :file, {
-      :docroot => "test/fixtures/var/lib/jerakia",
-      :format => :yaml,
+      :docroot    => "test/fixtures/var/lib/jerakia/data",
+      :format     => :yaml,
       :searchpath => [
         "host/#{scope[:hostname]}",
         "env/#{scope[:env]}",
