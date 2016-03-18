2d3a6098a9810815c2808780c23ff4646bbcfb0b
Craig Dunn <craig@craigdunn.org>
2016-03-18 11:28:07 +0100

    added schema aliasing

--- lib/jerakia/schema.rb
+++ lib/jerakia/schema.rb
@@ -20,6 +20,13 @@ class Jerakia::Schema
     @schema_data = schema_lookup.answer.payload || {}
     Jerakia.log.debug("Schema returned #{@schema_data}")
 
+    if salias = @schema_data["alias"]
+      Jerakia.log.debug("Schema alias found to #{@schema_data["alias"]}")
+      request.namespace=Array(salias["namespace"]) if salias["namespace"]
+      request.key = salias["key"] if salias["key"]
+    end
+
+
     if @schema_data["cascade"]
       Jerakia.log.debug("Overriding lookup_type from #{request.lookup_type} to :cascade")
       request.lookup_type= :cascade
