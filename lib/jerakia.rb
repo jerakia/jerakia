516471274ec8ae0cc852d536c8743a6e5203656c
Craig Dunn <craig@craigdunn.org>
2016-09-27 14:51:40 +0200

    rubocop fixes

--- lib/jerakia.rb
+++ lib/jerakia.rb
@@ -1,5 +1,9 @@
-
+# Class Jerakia
+#
+# Main Jerakia class
+#
 class Jerakia
+  require 'english'
   require 'jerakia/policy'
   require 'jerakia/lookup'
   require 'jerakia/request'
@@ -11,72 +15,101 @@ class Jerakia
   require 'jerakia/version'
   require 'jerakia/error'
 
+  attr_reader :options
 
-  def initialize(options={})
+  class << self
+    attr_reader :config
+    attr_reader :log
+    attr_reader :filecache
+  end
 
-    configfile = options[:config] || ENV['JERAKIA_CONFIG'] || '/etc/jerakia/jerakia.yaml'
-    @@config = File.exist?(configfile) ? Jerakia::Config.load_from_file(configfile) : Jerakia::Config.new
+  @config = nil
+  @filecache = {}
+  @log = nil
 
+  def initialize(options = {})
+    @options = options
 
-    if @@config[:plugindir]
-      $LOAD_PATH << @@config[:plugindir] unless $LOAD_PATH.include?(@@config[:plugindir])
+    load_config
+    load_log_handler
+
+    if config[:plugindir]
+      $LOAD_PATH << config[:plugindir] unless $LOAD_PATH.include?(config[:plugindir])
     end
 
-    @@filecache = {}
-    loglevel = options[:loglevel] || @@config["loglevel"] || "info"
-    logfile = options[:logfile] || @@config["logfile"] || "/var/log/jerakia.log"
-
-    @@log = Jerakia::Log.new(loglevel.to_sym, logfile)
-    @@log.debug("Jerakia initialized")
-
+    log.debug('Jerakia initialized')
     Jerakia.log.verbose("Jerakia started. Version #{Jerakia::VERSION}")
   end
 
   def lookup(request)
-    begin
-      lookup_instance = Jerakia::Launcher.new(request)
-      lookup_instance.invoke_from_file
-      lookup_instance.answer
-    rescue Jerakia::Error => e
-      Jerakia.fatal(e.message, e)
-    end
+    lookup_instance = Jerakia::Launcher.new(request)
+    lookup_instance.invoke_from_file
+    lookup_instance.answer
+  rescue Jerakia::Error => e
+    Jerakia.fatal(e.message, e)
+  end
 
+  def self.fatal(msg, e)
+    stacktrace = e.backtrace.join("\n")
+    Jerakia.log.fatal msg
+    Jerakia.log.fatal "Full stacktrace output:\n#{$ERROR_INFO}\n\n#{stacktrace}"
+    raise e
+  end
+
+  def filecache
+    self.class.filecache
   end
 
   def config
-    @@config
+    self.class.config
   end
 
-  def self.fatal(msg,e)
-    stacktrace=e.backtrace.join("\n")
-    Jerakia.log.fatal msg
-    Jerakia.log.fatal "Full stacktrace output:\n#{$!}\n\n#{stacktrace}"
-    raise e
+  def log
+    self.class.log
   end
 
   def self.filecache(name)
     begin
-      @@filecache[name] ||= File.read(name)
+      @filecache[name] ||= File.read(name)
     rescue Errno::ENOENT => e
       raise Jerakia::Error, "Could not read file #{name}, #{e.message}"
     end
-    return @@filecache[name]
+    @filecache[name]
   end
 
-  def add_to_filecache(name,data)
-    @@filecache[name] = data
+  class << self
+    def config=(data)
+      @config ||= data
+    end
+
+    def log=(logger)
+      @log ||= logger
+    end
   end
 
-  def self.config
-    @@config
+  private
+
+  def configfile
+    options[:config] || ENV['JERAKIA_CONFIG'] || '/etc/jerakia/jerakia.yaml'
   end
 
-  def log
-    @@log
+  def config_instance
+    File.exist?(configfile) ? Jerakia::Config.load_from_file(configfile) : Jerakia::Config.new
   end
 
-  def self.log
-    @@log
+  def load_config
+    self.class.config = config_instance
   end
 
+  def loglevel
+    options[:loglevel] || config['loglevel'] || 'info'
+  end
+
+  def logfile
+    options[:logfile] || config['logfile'] || '/var/log/jerakia.log'
+  end
+
+  def load_log_handler
+    self.class.log = Jerakia::Log.new(loglevel.to_sym, logfile)
+  end
 end
--- lib/jerakia/datasource.rb
+++ lib/jerakia/datasource.rb
@@ -1,48 +1,50 @@
+# rubocop:disable Lint/Eval
+#
 require 'jerakia/cache'
-class Jerakia::Datasource 
+class Jerakia
+  class Datasource
+    require 'jerakia/response'
 
-  require 'jerakia/response'
-  attr_reader :response
-  attr_reader :options
-  attr_reader :lookup
+    attr_reader :response
+    attr_reader :options
+    attr_reader :lookup
 
-  def initialize(name, lookup, opts)
-    @response = Jerakia::Response.new(lookup)
-    @options = opts
-    @lookup = lookup
-    @name = name
-    begin
-      require "jerakia/datasource/#{name.to_s}"
-      eval "extend Jerakia::Datasource::#{name.to_s.capitalize}"
-    rescue LoadError => e
-      raise Jerakia::Error, "Cannot load datasource #{name.to_s} in lookup #{lookup.name}, #{e.message}"
+    def initialize(name, lookup, opts)
+      @response = Jerakia::Response.new(lookup)
+      @options = opts
+      @lookup = lookup
+      @name = name
+      begin
+        require "jerakia/datasource/#{name}"
+        eval "extend Jerakia::Datasource::#{name.capitalize}"
+      rescue LoadError => e
+        raise Jerakia::Error, "Cannot load datasource #{name} in lookup #{lookup.name}, #{e.message}"
+      end
     end
-  end
 
-
-  ## used for verbose logging
-  def whoami
-    "datasource=#{@name}  lookup=#{@lookup.name}"
-  end
-
-  def option(opt, data={})
-    if @options[opt].nil? and data.key?(:default)
-      @options[opt] = data[:default]
+    ## used for verbose logging
+    def whoami
+      "datasource=#{@name}  lookup=#{@lookup.name}"
     end
+
+    def option(opt, data={})
+      if @options[opt].nil? and data.key?(:default)
+        @options[opt] = data[:default]
+      end
     
-    Jerakia.log.debug("[#{whoami}]: options[#{opt}] to #{options[opt]} [#{options[opt].class}]")
-    if @options[opt].nil?
-      raise Jerakia::PolicyError, "#{opt} option must be supplied for datasource #{@name} in lookup #{lookup.name}" if data[:mandatory]
-    else 
-      if data[:type]
-        if Array(data[:type]).select { |t| @options[opt].is_a?(t) }.empty?
-          raise Jerakia::PolicyError, 
-            "#{opt} is a #{@options[opt].class} but must be a #{data[:type].to_s}  for datasource #{@name} in lookup #{lookup.name}"
+      Jerakia.log.debug("[#{whoami}]: options[#{opt}] to #{options[opt]} [#{options[opt].class}]")
+      if @options[opt].nil?
+        raise Jerakia::PolicyError, "#{opt} option must be supplied for datasource #{@name} in lookup #{lookup.name}" if data[:mandatory]
+      else 
+        if data[:type]
+          if Array(data[:type]).select { |t| @options[opt].is_a?(t) }.empty?
+            raise Jerakia::PolicyError, 
+              "#{opt} is a #{@options[opt].class} but must be a #{data[:type].to_s}  for datasource #{@name} in lookup #{lookup.name}"
+          end
         end
       end
     end
-  end
     
           
+  end
 end
-
