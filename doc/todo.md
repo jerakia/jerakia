f254798a719f6d3a1a4ded711bd3349697414b61
Craig Dunn <craig@craigdunn.org>
2017-01-17 18:48:43 +0100

    clean branch

--- doc/todo.md
+++
@@ -1,7 +0,0 @@
-## Todo ##
-
-There are still lots of things that need closing off before 0.0.1 goes live, including:
-
-  * Log handling
-  * Error handling
-
--- lib/jerakia/response/filter.rb
+++
@@ -1,9 +0,0 @@
-class Jerakia::Response
-  module Filter
-    def filter!(name, opts)
-      Jerakia::Util.autoload('response/filter', name)
-      instance_eval "extend Jerakia::Response::Filter::#{name.to_s.capitalize}"
-      instance_eval "self.filter_#{name} (#{opts})"
-    end
-  end
-end
