ee4c2691a6423f14ff27eed10baad39a3eab5ae6
Craig Dunn <craig@craigdunn.org>
2017-12-08 14:34:51 +0100

    Added dig output handler (#112)
    This adds an output handler called dig that lets you drill into
    returned hash results and pull out a key
    
    Eg: if a datasource returns;
    
    ```
    {
      "document" => {
        "settings" => {
          "foo" => "bar"
        }
      }
    }
    ```
    
    if we are only interested in the key foo (assuming the lookup key
    is foo) then we can dig this in the lookup with
    
      `output_filter :dig, [ 'document', 'settings', request.key ]`

---
+++ spec/features/filter_dig_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe Jerakia do
+  let(:subject) { Jerakia.new(:config =>  "#{JERAKIA_ROOT}/test/fixtures/etc/jerakia/jerakia.yaml") }
+  let(:request) { Jerakia::Request.new }
+  let(:answer) { subject.lookup(request) }
+
+  describe 'Digging values' do
+    context 'when looking up using the dig parameter' do
+      let(:request) do
+        Jerakia::Request.new(
+          key: 'tango',
+          policy: 'dig'
+        )
+      end
+
+      it 'should return a response' do
+        expect(answer).to be_a(Jerakia::Answer)
+      end
+
+      it 'should return the answer' do
+        expect(answer.payload).to eq('bar')
+      end
+
+      it 'should have a found state' do
+        expect(answer.found?).to eq(true)
+      end
+
+    end
+    context 'when a dug key is not found' do
+      let(:request) do
+        Jerakia::Request.new(
+          key: 'bad_key',
+          policy: 'dig'
+        )
+      end
+
+      it 'should return a response' do
+        expect(answer).to be_a(Jerakia::Answer)
+      end
+
+      it 'should return nil' do
+        expect(answer.payload).to eq(nil)
+      end
+
+      it 'should be set to not found' do
+        expect(answer.found?).to eq(false)
+      end
+
+    end
+
+  end
+end
---
+++ test/fixtures/etc/jerakia/policy.d/dig.rb
@@ -0,0 +1,31 @@
+policy :dig do
+
+  lookup :main do
+    datasource :dummy, {
+      :return => {
+        'document' =>  {
+          'settings' => {
+            'foo' => 'bar'
+          }
+        }
+      }
+    }
+
+    output_filter :dig, [ 'document', 'settings', request.key ]
+  end
+
+  lookup :main do
+    datasource :dummy, {
+      :return => {
+        'document' =>  {
+          'settings' => {
+            'tango' => 'bar'
+          }
+        }
+      }
+    }
+
+    output_filter :dig, [ 'document', 'settings', request.key ]
+  end
+end
+
